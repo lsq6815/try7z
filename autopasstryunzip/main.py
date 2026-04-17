40fb61f7b27942a19928cbedb79ce9a8e43e9d5d
lsq6815 <lsq6815@gmail.com>
2026-04-18 02:23:54 +0800

    style: capitalize help text for -h/--help option
    Make help message capitalization consistent with other options
    
    - Add add_help=False to ArgumentParser
    
    - Manually add -h/--help with capitalized help text

--- autopasstryunzip/main.py
+++ autopasstryunzip/main.py
@@ -296,6 +296,14 @@ def main() -> int:
     parser = argparse.ArgumentParser(
         prog="autopass-unzip",
         description="Auto-extract password-protected archives",
+        add_help=False,
+    )
+    parser.add_argument(
+        "-h",
+        "--help",
+        action="help",
+        default=argparse.SUPPRESS,
+        help="Show this help message and exit",
     )
     parser.add_argument(
         "-v",
