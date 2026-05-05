dfd045a79c851c901a577f3c2565a8890b3499c3
lsq6815 <lsq6815@gmail.com>
2026-05-05 21:42:18 +0800

    docs: suppress Sphinx warning for missing static directory

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ master_doc = 'index'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path = []
 
 # HTML theme options
 html_theme_options = {
